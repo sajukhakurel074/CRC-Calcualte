c201c3c573492fe1d1dede7b3a0c96aa78c7f6fc
074bex438.saju@pcampus.edu.np <074bex438.saju@pcampus.edu.np>
2024-07-23 11:28:45 +0545

    Show Filesize

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify, send_from_directory
 import binascii
 import os
 
-# from flask_cors import CORS
-# CORS(app)
-
 app = Flask(__name__, static_url_path='', static_folder='.')
 
 def calculate_crc(data):
@@ -19,18 +16,19 @@ def upload_file():
     if not files:
         return jsonify({'error': 'No selected files'})
 
-    crc_values = {}
+    file_details = {}
     for file in files:
         if file.filename == '':
             continue
         data = file.read()
         crc_value = calculate_crc(data)
-        crc_values[file.filename] = {
-            'decimal': crc_value,
-            'hex': hex(crc_value)
+        file_details[file.filename] = {
+            'size': len(data),
+            'decimal_crc': crc_value,
+            'hex_crc': hex(crc_value)
         }
 
-    return jsonify({'crc_values': crc_values})
+    return jsonify({'file_details': file_details})
 
 @app.route('/')
 def index():
